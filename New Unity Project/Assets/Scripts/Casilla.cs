245363ec9bdc5d33ee143b828f6576a187a47f12
TheFalc <35174379+TheFalc@users.noreply.github.com>
2018-04-20 13:22:21 -0300

    Proyecto de Unity -no zip
    Esto permite mayor accesibilidad a la hora de investigar archivos dentro de GitHub

---
+++ New Unity Project/Assets/Scripts/Casilla.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Casilla : MonoBehaviour {
+
+    public Material colorCasilla;
+    public int numCasilla;
+    void OnMouseDown()
+    {
+        print(numCasilla.ToString());
+    }
+
+   public void PonerColor(Material color)
+    {
+        GetComponent<MeshRenderer>().material = color;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/CrearCasillas.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CrearCasillas : MonoBehaviour {
+
+    public GameObject CasillaPrefab;
+    public int ancho, alto;
+    public Material Blanco, Negro;
+    private int cont = 0;
+    
+	public void Crear() {
+        for (int i = 0; i <= ancho; i++)
+        {
+            for (int k = 0; k <= alto; k++)
+            {
+                if ((i + k) % 2 == 0)
+                {
+                    CasillaPrefab.GetComponent<Casilla>().PonerColor(Blanco);
+                }
+                else
+                {
+                    CasillaPrefab.GetComponent<Casilla>().PonerColor(Negro);
+                }
+                CasillaPrefab.GetComponent<Casilla>().numCasilla = cont++;
+
+                GameObject casillaTemp = Instantiate(CasillaPrefab, new Vector3(i, k, 0),new Quaternion(0, -90, 90, 0)) as GameObject;
+               }
+        }
+    }
+}
